e50e9e3a02276a1a801b98b526d02bcf4f4a3bc5
Joao Vitor Borges <dev.joao@protonmail.com>
2025-02-27 05:45:44 -0300

    fix: imports on register endpoint

--- src/auth/services/register/register.service.ts
+++ src/auth/services/register/register.service.ts
@@ -2,17 +2,13 @@ import { RegisterDTO, AuthUserDTO } from "@/auth/dtos/auth.schema";
 import { Service } from "@/shared/interfaces/service.interface";
 import { PrismaRepository } from "@/shared/repositories/prisma.repository";
 import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
 import { hash } from "bcrypt";
-import { RefreshTokenService } from "../refresh-token.service";
 
 @Injectable()
 export class RegisterService implements Service {
     constructor(
         @Inject("PrismaRepository")
         private readonly prisma: PrismaRepository,
-        private readonly jwtService: JwtService,
-        private readonly refreshTokenService: RefreshTokenService,
     ) {}
 
     async execute(registerDto: RegisterDTO): Promise<AuthUserDTO> {
