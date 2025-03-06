7146a038289feca432c2e631e0d6203a8732622e
Joao Vitor Borges <dev.joao@protonmail.com>
2025-03-06 17:20:19 -0300

    feat: mailer service

--- app/src/config/env.ts
+++ app/src/config/env.ts
@@ -5,13 +5,8 @@ export const envSchema = z.object({
     PORT: z.coerce.number().optional().default(3000),
     CORS_ORIGIN: z.union([z.string(), z.array(z.string())]).default("*"),
     JWT_SECRET: z.string(),
-    GOOGLE_CLIENT_ID: z.string(),
-    GOOGLE_CLIENT_SECRET: z.string(),
-    GOOGLE_CALLBACK_URL: z.string(),
-    COOKIE_SECRET: z.string(),
-    GITHUB_CLIENT_ID: z.string(),
-    GITHUB_CLIENT_SECRET: z.string(),
-    GITHUB_CALLBACK_URL: z.string(),
+    MAILFLOW_URL: z.string(),
+    FRONTEND_URL: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
