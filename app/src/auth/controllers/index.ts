e28e5a82fffc766e677f26967c0b8e4cd7ada4f0
Joao Vitor Borges <dev.joao@protonmail.com>
2025-03-06 18:20:53 -0300

    feat: indexes update

--- app/src/auth/controllers/index.ts
+++ app/src/auth/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from "./login/login.controller";
 export * from "./register/register.controller";
 export * from "./refresh-token/refresh-token.controller";
+export * from "./request-password-reset/request-password-reset.controller";
+export * from "./reset-password/reset-password.controller";
--- app/src/auth/services/index.ts
+++ app/src/auth/services/index.ts
@@ -7,3 +7,6 @@ export * from "./generate-token/generate-token.service";
 export * from "./login/login.service";
 export * from "./register/register.service";
 export * from "./refresh-token/refresh-token.service";
+
+export * from "./request-password-reset/request-password-reset.service";
+export * from "./reset-password/reset-password.service";
